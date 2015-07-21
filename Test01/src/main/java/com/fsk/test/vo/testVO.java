43aec01aca192a0dfad6e4be453d867809a0be79
bk <bk@bkkang>
2015-07-21 13:38:14 +0900

    test

---
+++ Test01/src/main/java/com/fsk/test/vo/testVO.java
@@ -0,0 +1,19 @@
+package com.fsk.test.vo;
+
+public class testVO {
+	private String ename;
+	private String job;
+	
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+}
